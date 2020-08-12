625e6212f2e66ff2580a32760b4022c048997d49
jast90 <zhangzhiwen91@gmail.com>
2020-08-12 09:43:40 +0800

    comment example

---
+++ reacts/src/componnets/comment/index.jsx
@@ -0,0 +1,48 @@
+import React,{Component} from 'react'
+
+import CommentAdd from '../comment/add/comment-add'
+import CommentList from '../comment/list/comment-list'
+
+export default class App extends Component{
+    state = {
+        comments:[
+            {username:'Tom',content:'React挺好的！'},
+            {username:'Jack',content:'React太难了！'}
+        ]
+    }
+
+    addComment = (comment) => {
+        const {comments} = this.state
+        comments.unshift(comment)
+        this.setState({comments})
+    }
+
+    deleteComment = (index)=>{
+        const {comments} = this.state
+        comments.splice(index,1)
+        this.setState({comments})
+    }
+
+    render(){
+        const {comments} = this.state
+        return (
+            <div>
+                <header className="site-header jumbotron">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-xs-12">
+                                <h1>请发表对React的评论</h1>
+                            </div>
+                        </div>
+                    </div>
+                </header>
+                <div className="container">
+                    <div className="row">
+                        <CommentAdd addComment={this.addComment} />
+                        <CommentList comments={comments} deleteComment={this.deleteComment}/>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
---
+++ reacts/src/componnets/comment/item/comment-item.jsx
@@ -0,0 +1,30 @@
+import React, {Componnet, Component} from 'react'
+import PropTypes from 'prop-types'
+
+export default class CommnetItem extends Component{
+    static propTypes = {
+        comment: PropTypes.object.isRequired,
+        deleteComment: PropTypes.func.isRequired,
+        index: PropTypes.number.isRequired
+    }
+
+    handleClick = () => {
+        const {comment,deleteComment,index} = this.props
+        if(window.confirm(`确定删除${comment.username}的评价吗？`)){
+            deleteComment(index)
+        }
+    }
+
+    render(){
+        const {comment} = this.props
+        return (
+            <li className="list-group-item">
+                <div className="handle">
+                    <a href="javascript:;" onClick={this.handleClick}>删除</a>
+                </div>
+                <p className="user"><span>{comment.username}</span><span>说：</span></p>
+                <p className="contence">{comment.content}</p>
+            </li>
+        )
+    }
+}
